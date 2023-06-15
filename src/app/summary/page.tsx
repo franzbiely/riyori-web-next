394c55954632faf1a723835f193d84f0e3a06a52
Francis Albores <>
2023-06-15 14:30:20 +0800

    Update git

--- src/app/summary/page.tsx
+++ src/app/summary/page.tsx
@@ -1,11 +1,53 @@
-// This is the nt page summary screen
+// 8th
+
+import Innerpage from '@/components/innerpage';
+import QuantityField from '@/components/quantityField';
+import Search from '@/components/search';
+import Subinnerpage from '@/components/subinnerpage';
+import Table from '@/components/table';
 import Image from 'next/image';
-import styles from "./splash.module.css";
+import layout from './../../components/layout.module.css';
+import styles from "./summary.module.css";
 
 export default function Summary() {
     return (
-        <main className={styles.main}>
-            <Image className='image' src="/images/ryori-logo-icon.png" alt="Ryori" width={136} height={165} />
-        </main>
+        <Subinnerpage title='Summary'> 
+            <br />
+            <ul className={styles.list}>
+                {[1,2,3,4].map((item, key) => (
+                        <li className={`${styles.item} ${layout.container}`}>
+                            {/* <Link to="/item"> */}
+                            <Image className={`${styles.image} ${layout.column} ${layout.f2}`} src="/images/sample-chicken.png" alt="Ryori" width={45} height={35} />
+                            <div className={`${styles.qty} ${layout.column} ${layout.f1}`}>
+                                1 x
+                            </div>
+                            <div className={`${styles.item_meta} ${layout.column} ${layout.f6}`}>
+                                <h6>Chicken Combo 1</h6>
+                                <small>With riec and drink (12 oz)</small>
+                            </div>
+                            <div className={`${layout.column} ${layout.f1} ${styles.price}`}>P250</div>
+                            {/* </Link> */}
+                        </li>
+                    ))
+                }
+            </ul> 
+            <br />
+            <div className={`${layout.container} ${styles.subtotal}`}>
+                <div className={`${layout.column} ${layout.f6}`}>
+                    Subtotal
+                </div>
+                <div className={`${layout.column}`}>₱1,270.00</div>
+            </div>
+            <div className={layout.container}>
+                <div className={`${layout.column} ${layout.f6}`}>
+                    Total
+                </div>
+                <div className={`${layout.column}`}>₱1,270.00</div>
+            </div>
+            <br />
+            <button 
+                // onClick={handleClick} 
+            className="button-secondary">Pay</button>
+        </Subinnerpage>
     )
 }
