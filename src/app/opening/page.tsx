dc4688fc407120d01cf5f05c9592933877af7ff2
Francis Albores <>
2023-07-06 06:58:24 +0800

    Opening + Splash

--- src/app/opening/page.tsx
+++ src/app/opening/page.tsx
@@ -1,20 +1,17 @@
 // This is the second page opening screen
 import React from 'react';
 import Image from 'next/image';
+import 'animate.css'
 import styles from "./opening.module.css";
+import Link from 'next/link';
 
 export default function Opening() {
-    const handleClick = () => {
-        console.log('Button clicked!');
-    };
     return (
         <main className={styles.main}>
             <div className={styles.mainInner}>
-                <Image className='image' src="/images/ryori-brand.png" alt="Ryori" width={283} height={141} />
+                <Image className={styles.image} src="/images/ryori-brand.png" alt="Ryori" width={283} height={141} />
                 <br /><br />
-                <button 
-                    // onClick={handleClick} 
-                className="button-primary">Get Started</button>
+                <Link href="/menu"><button className="button-primary">Get Started</button></Link>
             </div>
         </main>
     )
--- src/app/splash/page.tsx
+++ src/app/splash/page.tsx
@@ -1,12 +1,21 @@
+"use client"
 // This is the first page splash screen
 import Image from 'next/image';
+import 'animate.css'
 import styles from "./splash.module.css";
+import { useEffect } from 'react';
+
 
 export default function Splash() {
-    
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            window.location.href="/opening"
+        }, 2000)
+        return () => clearTimeout(timer);
+    }, [])
     return (
         <main className={styles.main}>
-            <Image className='image' src="/images/ryori-logo-icon.png" alt="Ryori" width={136} height={165} />
+            <Image className={styles.image} src="/images/ryori-logo-icon.png" alt="Ryori" width={136} height={165} />
         </main>
     )
 }
