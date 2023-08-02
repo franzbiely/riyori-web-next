73720f85ea4402cb877583de61d2f6d1f1b95f7b
Elchen Tanauan <lchentanauan@gmail.com>
2023-08-02 15:34:17 +0800

    Update Payment toPayCash

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -1,134 +1,197 @@
 // 9th
-"use client"
-import Innerpage from '@/components/innerpage';
-import QuantityField from '@/components/quantityField';
-import Search from '@/components/search';
-import Subinnerpage from '@/components/subinnerpage';
-import Table from '@/components/table';
-import Image from 'next/image';
-import Link from 'next/link';
-import layout from './../../components/layout.module.css';
+"use client";
+import Innerpage from "@/components/innerpage";
+import QuantityField from "@/components/quantityField";
+import Search from "@/components/search";
+import Subinnerpage from "@/components/subinnerpage";
+import Table from "@/components/table";
+import Image from "next/image";
+import Link from "next/link";
+import layout from "./../../components/layout.module.css";
 import styles from "./payment.module.css";
-import TextField from '@mui/material/TextField';
-import { ChangeEvent, useState } from 'react';
+import TextField from "@mui/material/TextField";
+import { ChangeEvent, useState } from "react";
 
-var Window = {location:{search:'', href: ''}}
-if(typeof window !== 'undefined') {
-    Window = window
+var Window = { location: { search: "", href: "" } };
+if (typeof window !== "undefined") {
+  Window = window;
 }
 
 export default function Payment() {
-    const [openGCash, setOpenGCash] = useState(false)
-    const [gcashData, setGcashData] = useState({
-        phone: '',
-        name: '',
-        email: ''
-    })
-    const openGCashField = () => {
-        setOpenGCash(!openGCash)
+  const [openGCash, setOpenGCash] = useState(false);
+  const [gcashData, setGcashData] = useState({
+    phone: "",
+    name: "",
+    email: "",
+  });
+  const [payCash, setPayCash] = useState("to_pay");
+
+  const openGCashField = () => {
+    setOpenGCash(!openGCash);
+  };
+
+  const toPayCash = async () => {
+    const transaction_Id = localStorage.getItem("transaction_Id") || "";
+
+    const urlencoded = new URLSearchParams();
+    urlencoded.append("id", transaction_Id?.toString());
+    urlencoded.append("status", "to_pay_cash");
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/pos/transaction/${transaction_Id}`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: urlencoded,
+      }
+    );
+    // const data = await response.json();
+    // console.log(data);
+    // if (data.redirect) {
+    Window.location.href = "/rate";
+    // }
+  };
+
+  const handlePay = async () => {
+    const transaction_Id = localStorage.getItem("transaction_Id") || "";
+
+    const urlencoded = new URLSearchParams();
+    urlencoded.append("id", transaction_Id?.toString());
+    urlencoded.append("phone", gcashData.phone);
+    urlencoded.append("email", gcashData.email);
+    urlencoded.append("name", gcashData.name);
+
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/pos/transaction/create-payment/`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: urlencoded,
+      }
+    );
+
+    const data = await response.json();
+
+    if (data.redirect) {
+      Window.location.href = data.redirect;
     }
-    const handlePay = async () => {
-        const transaction_Id = localStorage.getItem('transaction_Id') || ''
+  };
 
-        const urlencoded = new URLSearchParams();
-        urlencoded.append("id", transaction_Id?.toString());
-        urlencoded.append("phone", gcashData.phone);
-        urlencoded.append("email", gcashData.email);
-        urlencoded.append("name", gcashData.name);
+  const handleOnChange = (element: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = element.target;
+    setGcashData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
 
-        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/pos/transaction/create-payment/`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: urlencoded
-        });
+  const inputStyles = {
+    borderRadius: "15px",
+  };
 
-        const data = await response.json();
+  return (
+    <Subinnerpage title="Payment">
+      <br />
+      <ul className={styles.list}>
+        <Link href="" onClick={toPayCash}>
+          <li className={`${styles.item} ${layout.container}`}>
+            {/* <Link to="/item"> */}
 
-        if(data.redirect) {
-            Window.location.href = data.redirect
-        }
-    }
-
-    const handleOnChange = (element: ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = element.target;
-        setGcashData((prevFormData) => ({
-            ...prevFormData,
-            [name]: value,
-        }));
-    }
-
-    const inputStyles = {
-        borderRadius: '15px',
-    };
-
-    return (
-        <Subinnerpage title='Payment'> 
-            <br />
-            <ul className={styles.list}>
-                <li className={`${styles.item} ${layout.container}`}>
-                    {/* <Link to="/item"> */}
-                    <Image className={`${layout.column}`} src="/images/cash-icon.png" alt="Ryori" width={33} height={18} />
-                    <div className={`${layout.column} ${layout.f7} ${styles.item_meta}`}>Cash</div>
-                    {/* </Link> */}
-                </li>
-                <li className={`${styles.item} ${layout.container}`}>
-                    {/* <Link to="/item"> */}
-                    <Image className={`${layout.column} `} src="/images/bank-cards.svg" alt="Ryori" width={30} height={28} />
-                    <div className={`${layout.column} ${layout.f7} ${styles.item_meta}`}>Debit/Credit</div>
-                    {/* </Link> */}
-                </li>
-                <Link href='' onClick={openGCashField}>
-                <li className={`${styles.item} ${layout.container}`}>
-                    <Image className={`${layout.column} `} src="/images/gcash.svg" alt="Ryori" width={62} height={62} />
-                    <div className={`${layout.column} ${layout.f7} ${styles.item_meta}`}>Gcash</div>
-                </li>
-                </Link>
-            </ul> 
-            <br />
-            <hr />
-            <br />
-            {openGCash && (
-                <>                    
-                    <p className={styles.p}>Enter Name: </p>
-                    <TextField                
-                        rows={1}
-                        variant="outlined"
-                        placeholder=""
-                        fullWidth
-                        name='name'
-                        sx={{ '& .MuiOutlinedInput-root': inputStyles }}
-                        onChange={handleOnChange}
-                    />
-                    <br /><br />
-                    <p className={styles.p}>Enter G-Cash Number: </p>
-                    <TextField                
-                        rows={1}
-                        variant="outlined"
-                        placeholder=""
-                        fullWidth
-                        name='phone'
-                        sx={{ '& .MuiOutlinedInput-root': inputStyles }}
-                        onChange={handleOnChange}
-                    />
-                    <br /><br />
-                    <p className={styles.p}>Enter Email: </p>
-                    <TextField                
-                        rows={1}
-                        variant="outlined"
-                        placeholder=""
-                        fullWidth
-                        name='email'
-                        sx={{ '& .MuiOutlinedInput-root': inputStyles }}
-                        onChange={handleOnChange}
-                    />
-                </> 
-            )}
-            <br /><br />
-            <button 
-                onClick={handlePay} 
-            className="button-secondary">Pay</button>
-        </Subinnerpage>
-    )
+            <Image
+              className={`${layout.column}`}
+              src="/images/cash-icon.png"
+              alt="Ryori"
+              width={33}
+              height={18}
+            />
+            <div
+              className={`${layout.column} ${layout.f7} ${styles.item_meta}`}
+            >
+              Cash
+            </div>
+            {/* </Link> */}
+          </li>
+        </Link>
+        <li className={`${styles.item} ${layout.container}`}>
+          {/* <Link to="/item"> */}
+          <Image
+            className={`${layout.column} `}
+            src="/images/bank-cards.svg"
+            alt="Ryori"
+            width={30}
+            height={28}
+          />
+          <div className={`${layout.column} ${layout.f7} ${styles.item_meta}`}>
+            Debit/Credit
+          </div>
+          {/* </Link> */}
+        </li>
+        <Link href="" onClick={openGCashField}>
+          <li className={`${styles.item} ${layout.container}`}>
+            <Image
+              className={`${layout.column} `}
+              src="/images/gcash.svg"
+              alt="Ryori"
+              width={62}
+              height={62}
+            />
+            <div
+              className={`${layout.column} ${layout.f7} ${styles.item_meta}`}
+            >
+              Gcash
+            </div>
+          </li>
+        </Link>
+      </ul>
+      <br />
+      <hr />
+      <br />
+      {openGCash && (
+        <>
+          <p className={styles.p}>Enter Name: </p>
+          <TextField
+            rows={1}
+            variant="outlined"
+            placeholder=""
+            fullWidth
+            name="name"
+            sx={{ "& .MuiOutlinedInput-root": inputStyles }}
+            onChange={handleOnChange}
+          />
+          <br />
+          <br />
+          <p className={styles.p}>Enter G-Cash Number: </p>
+          <TextField
+            rows={1}
+            variant="outlined"
+            placeholder=""
+            fullWidth
+            name="phone"
+            sx={{ "& .MuiOutlinedInput-root": inputStyles }}
+            onChange={handleOnChange}
+          />
+          <br />
+          <br />
+          <p className={styles.p}>Enter Email: </p>
+          <TextField
+            rows={1}
+            variant="outlined"
+            placeholder=""
+            fullWidth
+            name="email"
+            sx={{ "& .MuiOutlinedInput-root": inputStyles }}
+            onChange={handleOnChange}
+          />
+        </>
+      )}
+      <br />
+      <br />
+      <button onClick={handlePay} className="button-secondary">
+        Pay
+      </button>
+    </Subinnerpage>
+  );
 }
--- src/app/rate/page.tsx
+++ src/app/rate/page.tsx
@@ -1,133 +1,151 @@
-"use client"
+"use client";
 // 9th
 
-import Innerpage from '@/components/innerpage';
-import QuantityField from '@/components/quantityField';
-import Search from '@/components/search';
-import Subinnerpage from '@/components/subinnerpage';
-import Table from '@/components/table';
-import { Rating } from '@mui/material';
-import Image from 'next/image';
-import layout from './../../components/layout.module.css';
+import Innerpage from "@/components/innerpage";
+import QuantityField from "@/components/quantityField";
+import Search from "@/components/search";
+import Subinnerpage from "@/components/subinnerpage";
+import Table from "@/components/table";
+import { Rating } from "@mui/material";
+import Image from "next/image";
+import layout from "./../../components/layout.module.css";
 import styles from "./rate.module.css";
-import React, {ChangeEvent, SyntheticEvent, useEffect, useState} from 'react';
-import TextField from '@mui/material/TextField';
+import React, { ChangeEvent, SyntheticEvent, useEffect, useState } from "react";
+import TextField from "@mui/material/TextField";
 
-var Window = {location:{search:'', href: ''}}
-if(typeof window !== 'undefined') {
-    Window = window
+var Window = { location: { search: "", href: "" } };
+if (typeof window !== "undefined") {
+  Window = window;
 }
 
 export default function Rate() {
-    const [rating, setRating] = useState(0);
-    const [review, setReview] = useState('');
-    const [reviewSubmitted, setReviewSubmitted] = useState(false);
+  const [rating, setRating] = useState(0);
+  const [review, setReview] = useState("");
+  const [reviewSubmitted, setReviewSubmitted] = useState(false);
 
-    const [content, setContent] = useState({
-        title: 'Loading...',
-        content: 'We are now checking your transaction status'
-    })
+  const [content, setContent] = useState({
+    title: "Loading...",
+    content: "We are now checking your transaction status",
+  });
 
-    const handleRatingChange = (event: SyntheticEvent<Element, Event>, value: number | null) => {
-        setRating(value || 0);
-    };
+  const handleRatingChange = (
+    event: SyntheticEvent<Element, Event>,
+    value: number | null
+  ) => {
+    setRating(value || 0);
+  };
 
-    const init = async () => {
-        const transaction_Id = localStorage.getItem('transaction_Id')
-        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/pos/transaction/status/${transaction_Id}`);
-        const data = await response.json();
-        console.log({data})
-        if(data.status === 'succeeded') {
-            setContent({
-                title: 'Thank you!',
-                content: 'Thank you very much for your dining experience!'
-            }) 
-        }
-        if(data.status === 'awaiting_payment_method') {
-            setContent({
-                title: 'Awaiting Payment Method',
-                content: 'Please rescan the QR Code to settle your payment method first.'
-            }) 
-        }
-        localStorage.removeItem("orders");
-        localStorage.removeItem("table_Id");
-        localStorage.removeItem("store_Id");
+  const init = async () => {
+    const transaction_Id = localStorage.getItem("transaction_Id");
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/pos/transaction/status/${transaction_Id}`
+    );
+    const data = await response.json();
+    console.log({ data });
+    if (data.status === "succeeded") {
+      setContent({
+        title: "Thank you!",
+        content: "Thank you very much for your dining experience!",
+      });
     }
-
-    const handleOnChange = (element: ChangeEvent<HTMLInputElement>) => {
-        setReview(element.currentTarget.value)
+    if (data.status === "awaiting_payment_method") {
+      setContent({
+        title: "Awaiting Payment Method",
+        content:
+          "Please rescan the QR Code to settle your payment method first.",
+      });
     }
-
-    const handleClick = async () => {
-        const branch_Id = localStorage.getItem('branch_Id') || ''
-
-        const urlencoded = new URLSearchParams();
-        urlencoded.append("description", review);
-        urlencoded.append("rating", rating.toString());
-        urlencoded.append("branch_Id", branch_Id);
-
-        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/reviews`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: urlencoded
-        });
-        const data = await response.json();
-        localStorage.removeItem("branch_Id");
-        setReviewSubmitted(true)
+    if (data.status === "to_pay_cash") {
+      setContent({
+        title: "Thank you!",
+        content:
+          "Our Staff will confirm your payment in a moment. Please rate us for the meantime",
+      });
     }
+    localStorage.removeItem("orders");
+    localStorage.removeItem("table_Id");
+    localStorage.removeItem("store_Id");
+  };
 
-    useEffect(() => {
-        init();
-    },[])
+  const handleOnChange = (element: ChangeEvent<HTMLInputElement>) => {
+    setReview(element.currentTarget.value);
+  };
 
-    const inputStyles = {
-        borderRadius: '15px',
-    };
+  const handleClick = async () => {
+    const branch_Id = localStorage.getItem("branch_Id") || "";
 
-    return (
-        <Subinnerpage title={content.title}> 
-            <br /><br />
-            <div className={`${styles.rate_container} ${layout.container}`}>
-                <Image src="/images/green-circle.svg" alt="Thank You" width={120} height={120} />
-                <h1 className={layout.h1}>{content.title}</h1>
-                <h2>{content.content}</h2>
-            
-                <br />
-                {reviewSubmitted && (<h3>Thank you for submitting your review.</h3>)}
-                {!reviewSubmitted && (
-                    <>
-                        <h3>Please rate us</h3>
-                        <Rating
-                            name="star-rating"
-                            value={rating}
-                            onChange={handleRatingChange}
-                            max={5}
-                            precision={1}
-                            size="large"
-                            />
-                        <br />
-                        <p className={styles.p}>Do you have any thoughts you'd like to share?</p>
-                        <TextField
-                            multiline
-                            rows={4}
-                            variant="outlined"
-                            placeholder=""
-                            fullWidth
-                            onChange={handleOnChange}
-                            sx={{ '& .MuiOutlinedInput-root': inputStyles }}
-                        /> 
-                        <br />
-                        <button 
-                            onClick={handleClick} 
-                        className="button-secondary">Submit</button>
-                    </>
-                )}
-                
-            </div>
+    const urlencoded = new URLSearchParams();
+    urlencoded.append("description", review);
+    urlencoded.append("rating", rating.toString());
+    urlencoded.append("branch_Id", branch_Id);
 
-            
-        </Subinnerpage>
-    )
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/reviews`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: urlencoded,
+    });
+    const data = await response.json();
+    localStorage.removeItem("branch_Id");
+    setReviewSubmitted(true);
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  const inputStyles = {
+    borderRadius: "15px",
+  };
+
+  return (
+    <Subinnerpage title={content.title}>
+      <br />
+      <br />
+      <div className={`${styles.rate_container} ${layout.container}`}>
+        <Image
+          src="/images/green-circle.svg"
+          alt="Thank You"
+          width={120}
+          height={120}
+        />
+        <h1 className={layout.h1}>{content.title}</h1>
+        <h2>{content.content}</h2>
+
+        <br />
+        {reviewSubmitted && <h3>Thank you for submitting your review.</h3>}
+        {!reviewSubmitted && (
+          <>
+            <h3>Please rate us</h3>
+            <Rating
+              name="star-rating"
+              value={rating}
+              onChange={handleRatingChange}
+              max={5}
+              precision={1}
+              size="large"
+            />
+            <br />
+            <p className={styles.p}>
+              Do you have any thoughts you'd like to share?
+            </p>
+            <TextField
+              multiline
+              rows={4}
+              variant="outlined"
+              placeholder=""
+              fullWidth
+              onChange={handleOnChange}
+              sx={{ "& .MuiOutlinedInput-root": inputStyles }}
+            />
+            <br />
+            <button onClick={handleClick} className="button-secondary">
+              Submit
+            </button>
+          </>
+        )}
+      </div>
+    </Subinnerpage>
+  );
 }
