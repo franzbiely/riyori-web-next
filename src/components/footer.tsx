c7245c7fb06043cb77458b87fdca8aae33f0440f
Elchen Tanauan <lchentanauan@gmail.com>
2023-09-30 10:49:48 +0800

    Update Footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,21 +1,37 @@
 import styles from "./layout.module.css";
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <div className={styles.footer}>
       <div className={`${styles.container}`}>
-          <div className={styles.column}>
-            <Link href={'/menu'}><Image className='image' src="/images/home.svg" alt="Ryori" width={24} height={24} /></Link>
-          </div>
-          {/* <div className={styles.column}>
+        <div className={styles.column}>
+          <Link href={"/menu"}>
+            <Image
+              className="image"
+              src="/images/home.svg"
+              alt="Ryori"
+              width={24}
+              height={24}
+            />
+          </Link>
+        </div>
+        {/* <div className={styles.column}>
             <Image className='image' src="/images/qr-scan.svg" alt="Ryori" width={62} height={62} />          
           </div> */}
-          <div className={styles.column}>
-            <Link href={'/checkout'}><Image className='image' src="/images/basket-grey.svg" alt="Ryori" width={25} height={31} /></Link>
-          </div>
+        <div className={styles.column}>
+          <Link href={"/orders"}>
+            <Image
+              className="image"
+              src="/images/basket-grey.svg"
+              alt="Ryori"
+              width={25}
+              height={31}
+            />
+          </Link>
+        </div>
       </div>
     </div>
-  )
+  );
 }
