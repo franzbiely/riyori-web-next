6ef84ef43fb7b6e7838c31a794dd4e60eefe7b17
Elchen Tanauan <lchentanauan@gmail.com>
2023-10-20 15:21:01 +0800

    WIP Footer disableb cart button when no transaction_Id exist

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,6 +3,32 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Footer() {
+  const transaction_Id = localStorage.getItem("transaction_Id");
+
+  const footerContent = transaction_Id ? (
+    <div className={styles.column}>
+      <Link href={"/orders"}>
+        <Image
+          className="image"
+          src="/images/basket-grey.svg"
+          alt="Ryori"
+          width={25}
+          height={31}
+        />
+      </Link>
+    </div>
+  ) : (
+    <div className={styles.column}>
+      <Image
+        className="image"
+        src="/images/basket-grey.svg"
+        alt="Ryori"
+        width={25}
+        height={31}
+      />
+    </div>
+  );
+
   return (
     <div className={styles.footer}>
       <div className={`${styles.container}`}>
@@ -20,17 +46,7 @@ export default function Footer() {
         {/* <div className={styles.column}>
             <Image className='image' src="/images/qr-scan.svg" alt="Ryori" width={62} height={62} />          
           </div> */}
-        <div className={styles.column}>
-          <Link href={"/orders"}>
-            <Image
-              className="image"
-              src="/images/basket-grey.svg"
-              alt="Ryori"
-              width={25}
-              height={31}
-            />
-          </Link>
-        </div>
+        <div className={styles.column}>{footerContent}</div>
       </div>
     </div>
   );
